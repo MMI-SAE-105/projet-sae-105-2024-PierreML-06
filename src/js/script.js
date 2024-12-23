e858a0123e2fd9dfa7764266fdf8ba431b78d7ce
PierreML-06 <pierre.mouilleseaux-lhuillier@edu.univ-fcomte.fr>
2024-12-23 23:31:46 +0100

    img + début html + css + menu(pas fini)

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+// menu --------------------------------------------------------------------
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector(".menu");
+const page = document.body;
+
+// Vérifier si les éléments existent avant d'ajouter l'événement
+if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+        const isOpen = toggle.ariaExpanded === "true";
+        const isClosed = !isOpen;
+        toggle.ariaExpanded = isClosed;
+        nav.ariaHidden = isOpen;
+        page.classList.toggle("noscroll", isClosed);
+    });
+}
